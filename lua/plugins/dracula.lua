049291ea433b6e4b04ce6b73ff60d3318455c842
dai <langtuonline1412@gmail.com>
2025-09-21 01:12:15 -0700

    Cai noice thong bao

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,17 +1,26 @@
 -- lua/plugins/dracula.lua
 return {
-	"Mofiqul/dracula.nvim",
-	priority = 1000,
-	config = function()
-		require("dracula").setup({
-			colors = {
-				bg = "#282A36",
-				fg = "#F8F8F2",
-			},
-			show_end_of_buffer = true, -- hiển thị ~ ở dòng trống
-			transparent_bg = true, -- nền trong suốt
-		})
+    "Mofiqul/dracula.nvim",
+    priority = 1000,
+    config = function()
+        require("dracula").setup({
+            colors = {
+                bg = "#282A36",
+                fg = "#F8F8F2",
+            },
+            show_end_of_buffer = true, -- hiển thị ~ ở dòng trống
+            transparent_bg = true,     -- nền trong suốt
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        })
 
-		vim.cmd.colorscheme("dracula")
-	end,
+        vim.cmd.colorscheme("dracula")
+
+        require("notify").setup({
+            background_colour = "#00000000",
+        })
+    end,
 }
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,79 +1,95 @@
 return {
-	-- Linter - nvim-lint
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("lint").linters_by_ft = {
-				python = { "flake8" },
-				javascript = { "eslint_d" },
-				lua = { "luacheck" },
-			}
+    -- Linter - nvim-lint
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("lint").linters_by_ft = {
+                python = { "flake8" },
+                javascript = { "eslint_d" },
+                lua = { "luacheck" },
+            }
 
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
 
-	-- Formatter - conform.nvim
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					python = { "black" },
-					lua = { "stylua" },
-					javascript = { "prettier" },
-				},
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
+    -- Formatter - conform.nvim
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    python = { "black" },
+                    lua = { "stylua" },
+                    javascript = { "prettier" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
+            })
+        end,
+    },
 
-	-- Comment Code
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-		keys = {
-			{ "gc", mode = { "n", "v" }, desc = "Toggle comment" },
-			{ "gb", mode = { "n", "v" }, desc = "Toggle block comment" },
-		},
-	},
+    -- Comment Code
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+        keys = {
+            { "gc", mode = { "n", "v" }, desc = "Toggle comment" },
+            { "gb", mode = { "n", "v" }, desc = "Toggle block comment" },
+        },
+    },
 
-	-- Markdown preview
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = { "markdown" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_auto_start = 0
-		end,
-	},
+    -- Markdown preview
+    {
+        "iamcco/markdown-preview.nvim",
+        ft = { "markdown" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_auto_start = 0
+        end,
+    },
 
-	{
-		"preservim/vim-markdown",
-		ft = { "markdown" },
-		init = function()
-			vim.g.vim_markdown_folding_disabled = 1
-		end,
-	},
+    {
+        "preservim/vim-markdown",
+        ft = { "markdown" },
+        init = function()
+            vim.g.vim_markdown_folding_disabled = 1
+        end,
+    },
 
-	{
-		"MeanderingProgrammer/markdown.nvim",
-		name = "render-markdown", -- tên rút gọn để gọi lại
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		ft = "markdown",
-		config = function()
-			require("render-markdown").setup({})
-		end,
-	},
+    {
+        "MeanderingProgrammer/markdown.nvim",
+        name = "render-markdown", -- tên rút gọn để gọi lại
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        ft = "markdown",
+        config = function()
+            require("render-markdown").setup({})
+        end,
+    },
+    -- Noice
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
 }
